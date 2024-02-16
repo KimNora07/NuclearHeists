15503452808bd1873e611b7c1cb60c4d5080ba30
jws2170 <mpart2077@gmail.com>
2024-02-16 20:40:36 +0900

    AI_FIX

--- Assets/JWS/script/AI.cs
+++ Assets/JWS/script/AI.cs
@@ -4,51 +4,207 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
+    Animator anim;
     public Rigidbody2D rb;
-    public float moveSpeed;
-    public Animator anim;
-    private Vector2 movedir;
-    private Vector2 lastmovedir;
+    public float speed;
+    public Transform[] point;
+    public float waitTime;
+    int currentPointIndex;
+    Vector2 dir;
+    private Vector2 Lastdir;
+    public bool once;
 
-    void Update()
+
+    public GameObject player;
+    public bool IsFind = false;
+    bool chase = false;
+    public float time;
+    bool OnAngle = false;
+    
+    public GameObject AnglePos;
+
+
+    
+
+    void Start()
     {
-
-        Anim();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
+    { 
+        if(OnAngle == true)
+        {
+            MakeRay();
+        }
+
+    }
+
+    void Update()
     {
-        MakeDir();
-        Move();
+        checkAngle();
+        
+        Anim();
+        MakeDirToPlayer();
+
+        if(chase == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        if(chase == false)
+        {
+            MakeDir();
+
+            if (transform.position != point[currentPointIndex].position)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, point[currentPointIndex].position, speed * Time.deltaTime);
+                anim.SetBool("IsRun", true);
+            }
+            else
+            {
+                if (once == false)
+                {
+                    once = true;
+                    StartCoroutine(Wait());
+
+                }
+
+            }
+        }
+
+
+
+    }
+    IEnumerator Wait()
+    {
+        anim.SetBool("IsRun", false);
+        yield return new WaitForSeconds(waitTime);
+        if (currentPointIndex + 1 < point.Length)
+        {
+            currentPointIndex++;
+
+        }
+        else
+        {
+            currentPointIndex = 0;
+        }
+        once = false;
+
     }
 
     void MakeDir()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        dir = ((Vector2)point[currentPointIndex].position - (Vector2)transform.position).normalized;
 
-        if ((x == 0 && y == 0) && movedir.x != 0 || movedir.y != 0)
+
+        if (dir.x != 0 || dir.y != 0)
         {
-            lastmovedir = movedir;
+            Lastdir = dir;
         }
-
-        movedir = new Vector2(x, y).normalized;
     }
 
-    void Move()
+    void MakeDirToPlayer()
     {
-        //rb.velocity = new Vector2(movedir.x * moveSpeed, movedir.y * moveSpeed);
+        dir = ((Vector2)player.transform.position - (Vector2)transform.position).normalized;
 
-        rb.velocity = movedir * moveSpeed;
+        if(dir.x != 0 || dir.y != 0)
+        {
+            Lastdir = dir;
+        }
     }
 
     void Anim()
     {
-        anim.SetFloat("X", movedir.x);
-        anim.SetFloat("Y", movedir.y);
-        anim.SetFloat("Move", movedir.magnitude);
-        anim.SetFloat("lastMX", lastmovedir.x);
-        anim.SetFloat("lastMY", lastmovedir.y);
+        anim.SetFloat("AIx", dir.x);
+        anim.SetFloat("AIy", dir.y);
+        anim.SetFloat("LastAIx", Lastdir.x);
+        anim.SetFloat("LastAIy", Lastdir.y);
     }
 
+    void MakeRay()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
+        if (ray.collider != null)
+        {
+            IsFind = ray.collider.CompareTag("Player");
+            if (IsFind)
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+                time += Time.deltaTime;
+                if (time >= 1.5f && chase == false)
+                {
+                    time = 0;
+                    chase = true;
+                }
+
+
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+                chase = false;
+                time = 0;
+                OnAngle = false;
+
+            }
+        }
+    }
+
+    void checkAngle()
+    {
+        if (dir.y >= 0.9)
+        {
+            AnglePos.transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+
+
+        if (dir.x >= 0.9)
+        {
+            AnglePos.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+
+
+        if (dir.y <= -0.9)
+        {
+            AnglePos.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+
+        if (dir.x <= -0.9)
+        {
+            AnglePos.transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+
+
+        //if (dir.x > 0 || dir.y != 0)
+        //{
+        //    AnglePos.transform.rotation = Quaternion.Euler(0, 0, 180);
+        //}
+
+
+        //if (dir.x > 0 || dir.y != 0)
+        //{
+        //    AnglePos.transform.rotation = Quaternion.Euler(0, 0, 180);
+        //}
+
+
+        //if (dir.x > 0 || dir.y != 0)
+        //{
+        //    AnglePos.transform.rotation = Quaternion.Euler(0, 0, 180);
+        //}
+
+
+        //if (dir.x > 0 || dir.y != 0)
+        //{
+        //    AnglePos.transform.rotation = Quaternion.Euler(0, 0, 180);
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnAngle = true;
+        }
+    }
 }
